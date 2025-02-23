0135c1d9ba395ef2e802b5514257af70cc72d8ef
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-02-24 03:40:16 +0530

    updation

--- TUF-SHEET/01_N-Forest.cpp
+++ TUF-SHEET/01_N-Forest.cpp
@@ -1,7 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
+/*
+----------------------------------------------------------------
+Sample Input 1:
+3
 
+Sample Output 1:
+* * *
+* * *
+* * *
+
+Explanation Of Sample Input 1 :
+For N = 3, fill all the rows and columns in 3x3 matrix with ‘*’.
+----------------------------------------------------------------
+Sample Input 2 :
+1
+
+Sample Output 2 :
+*
+
+----------------------------------------------------------------
+Sample Input 3 :
+4
+
+Sample Output 3 :
+* * * *
+* * * *
+* * * *
+* * * *
+
+----------------------------------------------------------------
+*/
 
 void nForest(int n) {
 
