b4a7b34e9dda48f405f26298dbbf8f6b2f6f1eb8
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-02-24 03:46:31 +0530

    updation

--- TUF-SHEET/04_Triangle.cpp
+++ TUF-SHEET/04_Triangle.cpp
@@ -3,6 +3,37 @@ using namespace std ;
 
 
 
+/*
+----------------------------------------------------------------
+Sample Input 1:
+3
+
+Sample Output 1:
+1 
+2 2 
+3 3 3
+
+----------------------------------------------------------------
+Sample Input 2 :
+1
+
+Sample Output 2 :
+1
+
+----------------------------------------------------------------
+Sample Input 3 :
+5
+
+Sample Output 3 :
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+----------------------------------------------------------------
+*/
+
 void nTriangle(int n) {
 
     for(int i = 1 ; i <= n ; ++i ){
@@ -17,7 +48,7 @@ void nTriangle(int n) {
     
 int main(int argc , char const *argv[]){
     
-    int n = 4 ; 
+    int n = 5 ; 
 
 
     nTriangle(n) ;
