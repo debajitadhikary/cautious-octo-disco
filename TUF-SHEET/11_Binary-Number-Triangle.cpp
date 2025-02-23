e160bfe47f7323b077ea8a2e9d46d43b41c60c3d
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-02-24 04:01:56 +0530

    updation

--- TUF-SHEET/11_Binary-Number-Triangle.cpp
+++ TUF-SHEET/11_Binary-Number-Triangle.cpp
@@ -2,6 +2,41 @@
 using namespace std ;
 
 
+/*
+----------------------------------------------------------------
+Sample Input 1:
+3
+
+Sample Output 1:
+1
+0 1
+1 0 1
+
+----------------------------------------------------------------
+Sample Input 2 :
+4
+
+Sample Output 2 :
+1
+0 1
+1 0 1
+0 1 0 1
+
+----------------------------------------------------------------
+Sample Input 3 :
+6
+
+Sample Output 3 :
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+0 1 0 1 0 1 
+
+----------------------------------------------------------------
+*/
+
 
 void nBinaryTriangle(int n) {
 
@@ -22,7 +57,7 @@ void nBinaryTriangle(int n) {
 
 int main(int argc , char const *argv[]){
   
-    int n = 5 ; 
+    int n = 6 ; 
 
 
     nBinaryTriangle(n) ;
