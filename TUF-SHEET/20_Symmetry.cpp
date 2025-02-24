46c7c77823d9d8a01c0af7644668349c3a4b1b36
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-02-24 13:17:05 +0530

    updation

--- TUF-SHEET/20_Symmetry.cpp
+++ TUF-SHEET/20_Symmetry.cpp
@@ -1,4 +1,7 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include<iostream>
+
+
 using namespace std ;
 
 
@@ -8,80 +11,82 @@ Sample Input 1:
 3
 
 Sample Output 1:
-* * * * * * 
-* *     * * 
-*         * 
-*         * 
-* *     * * 
-* * * * * * 
+*         *
+* *     * *
+* * * * * *
+* *     * *
+*         *
 
 ----------------------------------------------------------------
 Sample Input 2 :
 1
 
 Sample Output 2 :
-* * 
-* * 
+*
 
 ----------------------------------------------------------------
 */
 
+
 void symmetry(int n) {
-    
-    int total = n * 4 - 1 ;
+
+
+    int total_chars = n * 4 - 1 ;
 
     // upper part : 
-    for(int i = n ; i >= 1 ; --i ){
+    for(int i = 1 ; i <= n ; ++i ){
 
-        int curr_chars = 2 * i + ( i - 1 ) * 2 +1 ;
+        int curr_chars = i * 2 + ( i - 1)* 2 + 1 ; 
 
         for(int j = 1 ; j <= i ; ++j ){
-            cout << "* " ;
-
+            cout << "* " ; 
         }
         
-        int space = total - curr_chars ;
-        
-        for(int k = 1 ; k <= space ; ++k ) cout << ' ' ;
+        int whitespace = total_chars - curr_chars ;
+
+        for(int k = 1 ; k <= whitespace ; ++k ){
+            cout << ' ' ;
+        }
+
         
         for(int j = 1 ; j <= i ; ++j ){
-            cout << "* " ;
-            
+            cout << "* " ; 
         }
         
-        // cout << " | " << space << " " << curr_chars ;
         cout << '\n' ;
+
     }
 
 
     // lower part : 
-    for(int i = 1 ; i <= n ; ++i ){
+    for(int i = n-1 ; i >= 1 ; --i ){
 
-        int curr_chars = 2 * i + ( i - 1 ) * 2 +1 ;
+        int curr_chars = i * 2 + ( i - 1)* 2 + 1 ; 
 
         for(int j = 1 ; j <= i ; ++j ){
-            cout << "* " ;
-
+            cout << "* " ; 
         }
         
-        int space = total - curr_chars ;
-        
-        for(int k = 1 ; k <= space ; ++k ) cout << ' ' ;
+        int whitespace = total_chars - curr_chars ;
+
+        for(int k = 1 ; k <= whitespace ; ++k ){
+            cout << ' ' ;
+        }
+
         
         for(int j = 1 ; j <= i ; ++j ){
-            cout << "* " ;
-            
+            cout << "* " ; 
         }
         
-        // cout << " | " << space << " " << curr_chars ;
         cout << '\n' ;
-    }
-    
-}
 
+    }
+
+}
 
 int main(int argc , char const *argv[]){
   
+
     int n = 3 ; 
 
     symmetry(n) ;
