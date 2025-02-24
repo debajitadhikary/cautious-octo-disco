23634981adf520975bc04da5980131fb966b38c0
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2025-02-24 16:19:58 +0530

    updation

--- TUF-SHEET/22_Ninja-And-The-Number-Pattern.cpp
+++ TUF-SHEET/22_Ninja-And-The-Number-Pattern.cpp
@@ -48,14 +48,67 @@ Sample Output 3 :
 
 void getNumberPattern(int n) {
 
+    const int col = 2 * n - 1 ;
 
+    vector<vector<int>> dp ( col , vector<int> (col , 0)) ;
+
+    int left = 0 , top = 0 , right = col -1 , bottom = col-1 ;
+
+    int bit = n ;
+
+    while( left <= right && top <= bottom ){
+
+        // left to right : 
+        
+        for(int i = left ; i <= right ; ++i ){
+            dp[top][i] = bit ;
+        }
+        ++top ; 
+        
+        
+        // right to bottom : 
+        
+        for(int i = top ; i <= bottom ; ++i ){
+            dp[i][right] = bit ;
+        }
+        --right ;
+        
+        
+        // bottom to left : 
+        if( top <= bottom ){
+            for(int i = right ; i >= left ; --i ){
+                dp[bottom][i] = bit ;
+            }
+            --bottom ;
+        }
+        
+        
+        // left to top : 
+        if( left <= right){
+            for(int i = bottom ; i >= top ; --i ){
+                dp[i][left] = bit ;
+            }
+            ++left ;
+        }
+        
+        --bit ;
+    }
+        
+        
+    // print the final grid :
+    for(const auto &it : dp ){
+        for(const auto &i : it ){
+            cout << i ;
+        }
+        cout << '\n' ;
+    }
 
 }
 
 
 int main(int argc , char const *argv[]){
     
-    int n = 4 ; 
+    int n = 5 ; 
 
     getNumberPattern(n) ;
 
